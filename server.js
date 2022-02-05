9deec5d73dde76bade0ccdb168cee773b92c9808
Michael Walker <michaelwwalker42@gmail.com>
2022-02-04 20:16:15 -0700

    set up user-routes

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use(require('./controllers'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
 
