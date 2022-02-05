c7985bf854f6dd7c05dd794ae67327192cdf865d
Michael Walker <michaelwwalker42@gmail.com>
2022-02-04 19:54:32 -0700

    add user seeds

---
+++ seeds/user-seeds.js
@@ -0,0 +1,28 @@
+const { User } = require('../models');
+
+const userdata = [
+  {
+    username: 'alesmonde0',
+    password: 'password123'
+  },
+  {
+    username: 'jwilloughway1',
+    password: 'password123'
+  },
+  {
+    username: 'iboddam2',
+    password: 'password123'
+  },
+  {
+    username: 'dstanmer3',
+    password: 'password123'
+  },
+  {
+    username: 'djiri4',
+    password: 'password123'
+  }
+];
+
+const seedUsers = () => User.bulkCreate(userdata, { individualHooks: true });
+
+module.exports = seedUsers;
