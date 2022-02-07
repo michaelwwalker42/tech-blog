e06d28a355a7fc342962222ed4189a582826bccb
Michael Walker <michaelwwalker42@gmail.com>
2022-02-06 18:42:13 -0700

    add associations for comments

--- models/index.js
+++ models/index.js
@@ -3,6 +3,9 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 // create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
 Post.belongsTo(User, {
   foreignKey: 'user_id',
@@ -14,8 +17,18 @@ Comment.belongsTo(User, {
   onDelete: 'SET NULL'
 });
 
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
 Post.hasMany(Comment, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post };
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+})
+
+module.exports = { User, Post, Comment };
